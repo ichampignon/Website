83942d8c622007ba60f13d6ea66db5aa1f212799
champ <67729802+ichampignon@users.noreply.github.com>
2022-11-10 08:14:21 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+# please install streamlit first
+# Find more emojis here https://www.webfx.com/tools/emoji-cheat-sheet/
+# Down Below is title of the page
+st.set_page_config(page_title="My Webpage", page_icon=":umbrella:")
+
+#---- READ -----
+# ---- HEADER SECTION DOWN BELOW ----
+with st.container():
+    st.subheader("Hi There, I Am Champignon :satisfied:")
+    st.title("A Free Time Developer :wrench:")
+    st.write("I Program Stuff When im Bored Using Python :snake:")
+    st.write("[Find More >](https://github.com/iChampignon)")
+
+
